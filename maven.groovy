116f62b7939a5d78e95da751fcf4abd5188733f0
Hernan Moreno <hernan.alexis.moreno@gmail.com>
2022-02-04 11:25:39 -0300

    Se sube tiempo de 60 a 80

--- maven.groovy
+++ maven.groovy
@@ -23,10 +23,10 @@ def call() {
             sh 'mvn clean verify sonar:sonar -Dsonar.projectKey=ejemplo-gradle -Dsonar.java.binaries=build'
         }
     }
-    env.TAREA = 'Paso 5: Curl Springboot Maven sleep 60'
+    env.TAREA = 'Paso 5: Curl Springboot Maven sleep 80'
     stage("${env.TAREA}") {
         sh 'maven bootRun&'
-        sh "sleep 60 && curl -X GET 'http://localhost:8081/rest/mscovid/test?msg=testing'"
+        sh "sleep 80 && curl -X GET 'http://localhost:8081/rest/mscovid/test?msg=testing'"
     }
     env.TAREA = 'Paso 6: Subir Nexus'
     stage("${env.TAREA}") {
